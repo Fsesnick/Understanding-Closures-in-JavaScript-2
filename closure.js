d900bf52b0340c029b9568db907d9b310c11761c
Fsesnick <fernandasesnick@gmail.com>
2021-01-30 19:05:17 +0000

    function counter inside countBirds

--- closure.js
+++ closure.js
@@ -1,28 +1,16 @@
-/*var birds = 3;
 
-function dogHouse() {
-	var dogs = 8;
-  //console.log(birds);//3
-  //console.log(dogs);//8  
-  function showDogs(){
-    console.log(dogs);
-  }
-  return showDogs;
-}
-  //console.log(birds);//3
-  //console.log(dogs);//undefined 
-var getDogs = dogHouse();
-
-getDogs; //8
-*/
 var count = 0;
 
 var dogCount = 0;
 var birdCount = 0;
 
 function countBirds() {
-  count += 1;
-  return count + ' birds';
+  var count = 0;
+  function counter(){
+    count += 1;
+    return count + 'birds';
+  }
+  return counter;
 }
 
 function countDogs() {
