ef38f5d0a2a8bf34a54dcd31a0af21ab5d940e27
Fsesnick <fernandasesnick@gmail.com>
2021-01-30 19:08:09 +0000

    function MakeBirdCounter

--- closure.js
+++ closure.js
@@ -4,13 +4,12 @@ var count = 0;
 //var dogCount = 0;
 //var birdCount = 0;
 
-function countBirds() {
+function makeBirdCounter() {
   var count = 0;
-  function counter(){
+return  function(){
     count += 1;
     return count + 'birds';
   }
-  return counter;
 }
 
 function countDogs() {
