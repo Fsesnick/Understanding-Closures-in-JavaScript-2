3cf2882214200282c2869f0d6de3c1600dfe260a
Fsesnick <fernandasesnick@gmail.com>
2021-01-30 18:52:54 +0000

    function showDogs

--- closure.js
+++ closure.js
@@ -2,8 +2,15 @@ var birds = 3;
 
 function dogHouse() {
 	var dogs = 8;
-  console.log(birds);//3
-  console.log(dogs);//8  
+  //console.log(birds);//3
+  //console.log(dogs);//8  
+  function showDogs(){
+    console.log(dogs);
+  }
+  return showDogs;
 }
-  console.log(birds);//3
-  console.log(dogs);//undefined 
+  //console.log(birds);//3
+  //console.log(dogs);//undefined 
+var getDogs = dogHouse();
+
+getDogs; //8
